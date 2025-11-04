b1272addded78c45712ea2212df1d26df9e420da
Sofia <smazzini@itba.edu.ar>
2025-11-04 15:17:03 -0300

    cambios compile

--- compile.sh
+++ compile.sh
@@ -1,8 +1,9 @@
 =====================================================================
 #!/bin/bash
 
-sudo docker start TPE2
-sudo docker exec -it TPE2 make clean -C /root/Toolchain
-sudo docker exec -it TPE2 make clean -C /root/
-sudo docker exec -it TPE2 make -C /root/Toolchain
-sudo docker exec -it TPE2 make -C /root/
+docker start TPE2
+docker exec -u $USER -it TPE2 make clean -C /root/Toolchain
+docker exec -u $USER -it TPE2 make clean -C /root/
+docker exec -u $USER -it TPE2 make -C /root/Toolchain
+docker exec -u $USER -it TPE2 make -C /root/
+docker stop TPE2
